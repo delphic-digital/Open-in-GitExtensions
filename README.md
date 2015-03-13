56e5d19936cea344bf996cdac9ad4afcf7c9b337
Jeff <jmills@delphicdigital.com>
2015-03-13 10:32:25 -0400

    Add everything

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Open-in-GitExtensions
 Sublimetext 3 GitExtensions plugin
 
-Unzip it to your packages directory and make sure Git Extensions is in your environment paths: 
+Make sure Git Extensions executavle is in your environment paths:
 
 Add to Path:
 C:\Program Files (x86)\GitExtensions
 
-Unzip here:
+Clone here:
 C:\Users\[username]\AppData\Roaming\Sublime Text 3\Packages
---
+++ git_extensions.py
@@ -0,0 +1,56 @@
+import sublime, sublime_plugin
+from subprocess import Popen
+
+class git_extensions_browse(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'browse', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+
+class git_extensions_pull(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'pull', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+
+class git_extensions_commit(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'commit', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+
+class git_extensions_history(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'filehistory', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+
+class git_extensions_checkout(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'checkout', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+
+class git_extensions_merge(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'merge', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+
+class git_extensions_push(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'push', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+    
+class git_extensions_branch(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'branch', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
+      
+class git_extensions_reset(sublime_plugin.WindowCommand):
+  def run(self, paths):
+    args = [ 'reset', "%s" % paths[0]]
+    Popen(['GitExtensions'] + args)
+    return None
